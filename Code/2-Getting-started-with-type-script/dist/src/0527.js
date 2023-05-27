f55b73917396b76831798f7312327d9d747a7992
Mahmoud-Da <mdabbagh6@gmail.com>
2023-05-27 19:44:16 +0900

    finish 2 section

---
+++ Code/2-Getting-started-with-type-script/dist/src/0527.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello World");
+let age = 20;
+if (age > 10) {
+    age = +10;
+}
+//# sourceMappingURL=0527.js.map
---
+++ Code/2-Getting-started-with-type-script/src/0527.ts
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+let age: number = 20;
+
+if (age > 10){
+    age =+ 10
+}
