3bbcc5d25128379c061c8fe009235d3726d817ea
Mahmoud-Da <mdabbagh6@gmail.com>
2023-06-10 20:19:53 +0900

    lesson5_2

--- Code/dist/index5.js
+++ Code/dist/index5.js
@@ -1,20 +1,74 @@
 "use strict";
 class Account {
-    constructor(id, owner, balance) {
+    constructor(id, owner, _balance, nickname) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = _balance;
+        this.nickname = nickname;
     }
+    calculateTax() { }
     deposte(amount) {
         if (amount <= 0) {
             throw new Error("invalid amount");
         }
-        this.balance += amount;
+        this._balance += amount;
+        this.calculateTax();
+    }
+    get balance() {
+        return this._balance;
+    }
+    set balance(value) {
+        if (value > 0) {
+            throw new Error("invalid Value");
+        }
+        this._balance = value;
     }
 }
 let account = new Account(1, "moody", 0);
 account.deposte(100);
-console.log(account.balance);
 console.log(typeof account);
 console.log(account instanceof Account);
+class A {
+    constructor(id) {
+        this.id = id;
+    }
+}
+class SeatAssignment {
+}
+let seats = new SeatAssignment();
+seats.A1 = "Ahmad";
+seats["A1"] = "Ahmad";
+seats.A2 = "Muhammed";
+class Ride {
+    start() {
+        Ride._activeRides++;
+    }
+    stop() {
+        Ride._activeRides--;
+    }
+    static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+Ride._activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.stop();
+console.log(Ride.activeRides);
+class Person {
+    constructor(id, firstName, lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get fullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+class Student extends Person {
+    get fullName() {
+        return "Student" + super.fullName;
+    }
+}
 //# sourceMappingURL=index5.js.map
--- Code/src/index5.ts
+++ Code/src/index5.ts
@@ -1,22 +1,34 @@
 // Creating Classes
 class Account {
-  readonly id: number;
-  owner: string;
-  balance: number;
-  //   optional property
-  nickname?: string;
+  constructor(
+    // readonly (we can't modify)
+    public readonly id: number,
+    public owner: string,
+    // private (not accessable outside the class)
+    private _balance: number,
+    //   optional property
+    public nickname?: string
+  ) {}
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this.balance = balance;
-  }
-
+  private calculateTax(): void {}
   deposte(amount: number): void {
     if (amount <= 0) {
       throw new Error("invalid amount");
     }
-    this.balance += amount;
+    this._balance += amount;
+    this.calculateTax();
+  }
+  // if we wan to aceess a private method
+  get balance() {
+    return this._balance;
+  }
+
+  // to be able to modify a private method
+  set balance(value: number) {
+    if (value > 0) {
+      throw new Error("invalid Value");
+    }
+    this._balance = value;
   }
 }
 
@@ -24,7 +36,8 @@ class Account {
 let account = new Account(1, "moody", 0);
 account.deposte(100);
 
-console.log(account.balance);
+// we can't access it because it's private
+// console.log(account.balance);
 console.log(typeof account);
 console.log(account instanceof Account);
 
@@ -34,3 +47,83 @@ console.log(account instanceof Account);
 // to prevent that we can pass readonly keyword => we can only set it in the constructer
 
 // we can create an optional property using ? => nickname?: string
+
+// Acess Control Keyword
+// all the property are puplic by default
+
+// parameters properties
+// by using puplic keyword we can inisilaize the parameter automatically
+
+class A {
+  // readonly id: number;
+
+  constructor(public readonly id: number) {
+    // this.id = id;
+  }
+}
+
+// Getter And Setter
+// Get to acess a private (property/method)
+// Set to be able to modify a private (property/method)
+
+// index signcuture
+class SeatAssignment {
+  // A1, A2, ...
+  // Ahmad, Muhammed, ...
+
+  [seatNumber: string]: string;
+}
+
+let seats = new SeatAssignment();
+seats.A1 = "Ahmad";
+// or
+seats["A1"] = "Ahmad";
+seats.A2 = "Muhammed";
+
+// Static Members
+class Ride {
+  private static _activeRides: number = 0;
+
+  start() {
+    Ride._activeRides++;
+  }
+
+  stop() {
+    Ride._activeRides--;
+  }
+
+  static get activeRides() {
+    return Ride._activeRides;
+  }
+}
+
+let ride1 = new Ride();
+ride1.start();
+
+let ride2 = new Ride();
+ride2.stop();
+
+console.log(Ride.activeRides);
+
+// Inheritence
+
+// override
+// when we override a method we  use override key word (or it weill not conect to the method that we want to override it)
+
+class Person {
+  constructor(
+    public readonly id: number,
+    public firstName: string,
+    public lastName: string
+  ) {}
+
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+class Student extends Person {
+  override get fullName() {
+    return "Student" + super.fullName;
+  }
+}
