eb7fdb02695d9b5fe7418820a0bc71ff017848cd
cham1235 <hana171819@gmail.com>
2023-08-22 22:25:25 +0900

    option - files まとめ

--- Cham(self-stydy)/cham/code.ts
+++ Cham(self-stydy)/cham/code.ts
@@ -1,10 +1,32 @@
 // 最初 tsc --init
 {
     "compilerOptions": {
-//      公式のドキュメント：https://www.typescriptlang.org/docs/handbook/tsconfig-json.html
+//     公式のドキュメント：https://www.typescriptlang.org/docs/handbook/tsconfig-json.html
 
-//      公式ドキュメントの和訳：http://js.studio-kingdom.com/typescript/project_configuration/tsconfig_json
+//     公式ドキュメントの和訳：http://js.studio-kingdom.com/typescript/project_configuration/tsconfig_json
 //     公式のtsconfig.jsonのドキュメント: https://www.typescriptlang.org/tsconfig
+
+/*Project
+
+
+
+
+/*option オプション*/
+
+    /* files*/   //コンパイルするファイルを直接指定する. includeやexcludeと違ってワイルドカードを使用できない。
+                //グロブパターンを必要としない場合で有用する。 グロブパターンが必要な場合、includeを利用する。
+    "files": [
+        "core.ts",
+        "sys.ts",
+        "types.ts",
+        "scanner.ts",
+        "parser.ts",
+        "utilities.ts",
+        "binder.ts",
+        "checker.ts",
+        "tsc.ts"
+    ]
+
 
 
     }
