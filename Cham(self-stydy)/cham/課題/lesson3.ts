f57634116a2223ab5ada214fa7d2afddb5002424
cham1235 <hana171819@gmail.com>
2023-08-27 21:57:49 +0900

    object, tuble, enum まとめ

--- Cham(self-stydy)/cham/課題/lesson3.ts
+++ Cham(self-stydy)/cham/課題/lesson3.ts
@@ -1,6 +1,9 @@
 // JavaScript: [number, string, boolean, null, undefined, object]
 // TypeScript: [JavaScript same types + [any, unknown, never, enum, tuble]]
 
+// undefinedは「値が代入されていないため、値がない」
+// nullは「代入すべき値が存在しないため、値がない」
+
 /* number */
 // JS
 let number = 2345;
@@ -33,8 +36,92 @@ pending = true;
 // ..
 pending = false;
 
-/* any amything の意味：string も　number　も boolean　オーケー */
+/* any anything の意味：string も　number　も boolean　オーケー */
+// anyはTypeScriptが型のチェックを放棄した型です。anyの使用を避ける。エラーが実行しか発見しないから。
+// unknown はTypeScriptが型をチェックする。
+
 let result: any = true;
 result = "ok";
 result = 9;
 let sp1: any[] = [2, "Corddoba Guitar Guclcor", true];
+
+function getData(callback: any) {
+  callback();
+}
+getData("abc"); //TypeError: callback is not a function
+
+function getData1(callback1: unknown) {
+  callback1(); //callback1 is of type unknown
+}
+getData1("abc");
+
+/* enum は名前付き定数値のグループです。 Enum は列挙型を表す。*/
+enum Month {
+  Jan,
+  Feb,
+  Mar,
+  Apr,
+  May,
+  Jun,
+  Jul,
+  Aug,
+  Sep,
+  Oct,
+  Nov,
+  Dec,
+}
+function isItSummer(month: Month) {
+  let isSummer: boolean;
+  switch (month) {
+    case Month.Jun:
+    case Month.Jul:
+    case Month.Aug:
+      isSummer = true;
+      break;
+    default:
+      isSummer = false;
+      break;
+  }
+  return isSummer;
+}
+console.log(isItSummer(Month.Jun)); // true
+
+/* tuble */
+//配列と同じですが、各値を異なる型に初期化できる。
+let skill: [string, number];
+skill = ["Programming", 5];
+
+let skill1: [string, number];
+skill1 = [5, "Programming"]; //error TS2322: Type 'number' is not assignable to type 'string'.
+
+/*　object　*/
+let myDog: {
+  name: string;
+  age: number;
+  birthday: Date;
+  bark: () => void;
+} = {
+  name: "Fido",
+  age: 3,
+  birthday: new Date("1990-01-01"),
+  bark: () => console.log("Woof! Woof!"),
+};
+
+let car: {
+  make: string;
+  model: string;
+  year: number;
+  getFullName: () => string;
+} = {
+  make: "Toyota",
+  model: "Camry",
+  year: 2020,
+  getFullName: () => {
+    return `${car.year} ${car.make} ${car.model}`;
+  },
+};
+
+console.log(car.make); // "Toyota"
+console.log(car.model); // "Camry"
+console.log(car.year); // 2020
+console.log(car.getFullName()); // "2020 Toyota Camry"
