49ac07cb274dea155222c92b1ca5304be43a0160
cham1235 <hana171819@gmail.com>
2023-09-19 06:37:50 +0900

    修正と課題4

--- Cham(self-stydy)/cham/課題/課題.ts
+++ Cham(self-stydy)/cham/課題/課題.ts
@@ -1,8 +1,8 @@
 // Exercise 1: 変数とデータ型
 // 質問: age という変数を宣言し、それをあなたの年齢に設定してください。次に、name という変数を宣言し、それをあなたの名前に設定してください。
-let age = 18;
+let age: number = 18;
 
-let name = "Cham";
+let name: string = "Cham";
 
 // Exercise 2: 基本的な関数
 // 質問: add という TypeScript 関数を作成し、a と b という二つのパラメータを受け取り、それらの合計を返すようにしてください。
@@ -11,8 +11,8 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
-let monney = add(10, 20);
-console.log(monney); // 30
+let money: number = add(10, 20);
+console.log(money); // 30
 
 // Exercise 3: 配列
 // 質問: 三つの異なる果物の名前を含む fruits という名前の配列を宣言してください。その後、別の果物を配列に追加してください。
@@ -20,3 +20,14 @@ console.log(monney); // 30
 let fruits: String[] = ["Orange", "Grape", "Banana"];
 fruits.push("Apple");
 console.log(fruits); // [Orange, Grape, Banana, Apple]
+
+// Exercise 4: オブジェクト
+// 質問: name と age というプロパティを持つ person という名前のオブジェクトを作成してください。それらの値をあなたの名前と年齢に設定してください。
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Cham",
+  age: 32,
+};
