170ffba74a55006f3c893eea94a5f8cd77398298
cham1235 <hana171819@gmail.com>
2023-09-24 22:54:29 +0900

    課題1　途中

--- Cham(self-stydy)/cham/lesson/src/task_20230924.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20230924.ts
@@ -1,14 +1,14 @@
 // 課題１
 // 偶数かつ 1000 未満 の項を全て足した値を求め，表示させよ
 
-function Lolo(i: number): number {
-  if (i % 2 === 0) {
-    return i;
+function Lolo(number: number): number {
+  if (number % 2 === 0) {
+    return number;
   }
   return 0;
 }
 
-function sumLolo(): number[] {
+function sumLolo() {
   let sum: number = 0;
   for (let i = 0; i < 1000; i++) {
     sum += Lolo(i);
