2f9d173b2456067b2c9ed43673371c1a7db2da04
cham1235 <hana171819@gmail.com>
2023-09-27 20:28:27 +0900

    課題１修正＋課題2完成

--- Cham(self-stydy)/cham/lesson/src/task_20230924.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20230924.ts
@@ -1,17 +1,25 @@
 // 課題１
 // 偶数かつ 1000 未満 の項を全て足した値を求め，表示させよ
 
-function Lolo(number: number): number {
+function EvenNumber(number: number): number {
   if (number % 2 === 0) {
     return number;
   }
   return 0;
 }
 
-function sumLolo() {
+function SumEvenNumber() {
   let sum: number = 0;
   for (let i = 0; i < 1000; i++) {
-    sum += Lolo(i);
+    sum += EvenNumber(i);
   }
-  console.log(sum);
+  return sum;
 }
+
+// 課題２
+// 100, 255, 92, -324, 1982 を要素に持つリストを，大きい順に並び替えたリストを表示させよ
+
+let numArr = [100, 255, 92, -324, 1982];
+numArr.sort(function (x: number, y: number): number {
+  return y - x;
+});
