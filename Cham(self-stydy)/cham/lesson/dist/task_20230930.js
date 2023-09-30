f612801963b8b37b08f31a4b013ccf6010dec878
cham1235 <hana171819@gmail.com>
2023-09-30 22:33:28 +0900

    課題4　完成

---
+++ Cham(self-stydy)/cham/lesson/dist/task_20230930.js
@@ -0,0 +1,9 @@
+"use strict";
+let stringArr = ["hello", "hi", "mama", "papa"];
+stringArr.sort(function () {
+    return -1;
+});
+let stringArr1 = ["hello", "hi", "mama", "papa"];
+stringArr1.sort(() => -1);
+let stringArr2 = ["hello", "hi", "mama", "papa"];
+stringArr2.reverse();
