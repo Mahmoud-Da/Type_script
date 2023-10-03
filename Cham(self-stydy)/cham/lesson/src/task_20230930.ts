27f38d947a0712f58cfac6a3dc2a61875769b3aa
cham1235 <hana171819@gmail.com>
2023-10-03 20:38:46 +0900

    修正　完了

--- Cham(self-stydy)/cham/lesson/src/task_20230930.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20230930.ts
@@ -1,16 +1,16 @@
 // 課題4
 // 文字列を逆順にする: 文字列内の文字の順序を逆にするプログラムを作成してください。
 // .sort()
-let stringArr = ["hello", "hi", "mama", "papa"];
+let stringArr: string[] = ["hello", "hi", "mama", "papa"];
 stringArr.sort(function () {
   return -1;
 });
 
-let stringArr1 = ["hello", "hi", "mama", "papa"];
+let stringArr1: string[] = ["hello", "hi", "mama", "papa"];
 stringArr1.sort(() => -1); //Arrow funtion
 // test:['papa', 'mama', 'hi', 'hello']
 
 // .reverse()
-let stringArr2 = ["hello", "hi", "mama", "papa"];
+let stringArr2: string[] = ["hello", "hi", "mama", "papa"];
 stringArr2.reverse();
 // test:['papa', 'mama', 'hi', 'hello']
