f5c153090319cb8c571cf53af60208b55582c078
cham1235 <hana171819@gmail.com>
2023-10-04 23:11:18 +0900

    課題８　完成

---
+++ Cham(self-stydy)/cham/lesson/src/task_20231004.ts
@@ -0,0 +1,10 @@
+// 課題８
+// 文字列を入力として受け取り、逆の文字列を返す関数を作成してください。この関数は再帰的であり、問題を解決するために自身を呼び出します。
+function reverseString(string: string): string {
+  var newString: string = "";
+  for (var i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
+}
+reverseString("Hello");
