2079e70673b28509c78c58ca6448f96ad01a87d3
cham1235 <hana171819@gmail.com>
2023-10-08 02:35:32 +0900

    修正　完了

--- Cham(self-stydy)/cham/lesson/dist/task_20231004.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231004.js
@@ -1,7 +1,7 @@
 "use strict";
 function reverseString(string) {
-    var newString = "";
-    for (var i = string.length - 1; i >= 0; i--) {
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
         newString += string[i];
     }
     return newString;
