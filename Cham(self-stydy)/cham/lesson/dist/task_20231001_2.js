0fef931709b7d7759eb06d59f63cf3f84382a90b
cham1235 <hana171819@gmail.com>
2023-10-10 20:58:51 +0900

    修正　完成

--- Cham(self-stydy)/cham/lesson/dist/task_20231001_2.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231001_2.js
@@ -17,3 +17,5 @@ function prime(number) {
         }
     }
 }
+prime(5);
+prime(10);
--- Cham(self-stydy)/cham/lesson/src/task_20231001_2.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231001_2.ts
@@ -1,6 +1,6 @@
 // 課題６
 // 素数かどうかを確認する: 数字が素数かどうかを確認するプログラムを作成してください。素数とは、1と自分自身以外に約数を持たない数のことです。
-function prime(number: number) {
+function prime(number: number): boolean | undefined {
   if (number === 1) {
     return false;
   } else if (number === 2) {
@@ -16,3 +16,7 @@ function prime(number: number) {
     }
   }
 }
+
+// test
+prime(5); //true
+prime(10); //false
