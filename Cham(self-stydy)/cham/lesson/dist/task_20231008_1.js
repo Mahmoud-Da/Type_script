0796825e8050e48d4484acc32f3664d20035a583
cham1235 <hana171819@gmail.com>
2023-10-14 19:14:57 +0900

    複数修正

--- Cham(self-stydy)/cham/lesson/dist/task_20231008_1.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231008_1.js
@@ -1,17 +1,15 @@
 "use strict";
 function findLongestWord(string) {
+    if (!string) {
+        return "文を書いてください。";
+    }
     let array = string.match(/\w[a-z]{0,}/gi);
-    if (array === null) {
-        return "404";
-    }
-    else {
-        let result = array[0];
-        for (let i = 1; i < array.length; i++) {
-            if (result.length < array[i].length) {
-                result = array[i];
-            }
+    let result = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (result.length < array[i].length) {
+            result = array[i];
         }
-        return result;
     }
+    return result;
 }
 findLongestWord("I am LiLi, and I am a guinea pig");
--- Cham(self-stydy)/cham/lesson/src/task_20231008_1.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231008_1.ts
@@ -4,18 +4,19 @@
 // the_longest_word("hi madam lili") //output: madam
 
 function findLongestWord(string: string): string {
-  let array: string[] | null = string.match(/\w[a-z]{0,}/gi);
-  if (array === null) {
-    return "404";
-  } else {
-    let result: string = array[0];
-    for (let i = 1; i < array.length; i++) {
-      if (result.length < array[i].length) {
-        result = array[i];
-      }
-    }
-    return result;
+  if (!string) {
+    return "文を書いてください。";
   }
+
+  let array: RegExpMatchArray = string.match(/\w[a-z]{0,}/gi)!;
+
+  let result: string = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (result.length < array[i].length) {
+      result = array[i];
+    }
+  }
+  return result;
 }
 
 // test
