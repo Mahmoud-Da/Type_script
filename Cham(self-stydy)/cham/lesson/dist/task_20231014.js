b39ed7e13a36e54102fa5353dbd61674a1bde1c2
cham1235 <hana171819@gmail.com>
2023-10-15 15:18:07 +0900

    修正　完了

--- Cham(self-stydy)/cham/lesson/dist/task_20231014.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231014.js
@@ -1,5 +1,8 @@
 "use strict";
 function liliSearch(name) {
+    if (!name) {
+        return "文書を記入して下さい！";
+    }
     let array = name.match(/\w[a-z]{0,}/gi);
     let search = array.indexOf("lili");
     if (search === 0) {
@@ -11,6 +14,6 @@ function liliSearch(name) {
     return false;
 }
 liliSearch("my dog's name is mada");
-liliSearch("lili is my mouse");
-liliSearch("my lili is a female mouse");
+liliSearch("lili is my guinea pig");
+liliSearch("my lili is a female guinea pig");
 liliSearch("my mama love lili so much");
--- Cham(self-stydy)/cham/lesson/src/task_20231014.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231014.ts
@@ -10,7 +10,10 @@
 // Make a TypeScript program that accepts a string from the user as an input.
 // Display "True" if "lili" appears starting at index 0 or 1 in the string, otherwise display "False".
 
-function liliSearch(name: string): boolean {
+function liliSearch(name: string): boolean | string {
+  if (!name) {
+    return "文書を記入して下さい！";
+  }
   let array: RegExpMatchArray = name.match(/\w[a-z]{0,}/gi)!;
   let search: number = array.indexOf("lili");
   if (search === 0) {
@@ -24,6 +27,6 @@ function liliSearch(name: string): boolean {
 
 // test
 liliSearch("my dog's name is mada"); // false
-liliSearch("lili is my mouse"); // true
-liliSearch("my lili is a female mouse"); // true
+liliSearch("lili is my guinea pig"); // true
+liliSearch("my lili is a female guinea pig"); // true
 liliSearch("my mama love lili so much"); // false
