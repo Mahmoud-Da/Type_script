9d67560f63eb3cc2dff52c667e03526e8d2fc83f
cham1235 <hana171819@gmail.com>
2023-10-28 22:56:26 +0900

    finish

--- Cham(self-stydy)/cham/lesson/dist/task_20231025.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231025.js
@@ -1,12 +1,32 @@
 "use strict";
 let stack = [];
-stack.push(3);
+function add_number(array, number) {
+    if (array.length != 3 && typeof number != "string") {
+        array.push(number);
+    }
+    return array;
+}
+function stack_remove_number(array) {
+    if (array.length >= 1) {
+        array.pop();
+    }
+    return array;
+}
+add_number(stack, 1);
+add_number(stack, 2);
+add_number(stack, 3);
+add_number(stack, 3);
+stack_remove_number(stack);
 console.log(stack);
-stack.push(6);
-console.log(stack);
-stack.push(8);
-console.log(stack);
-stack.pop();
-console.log(stack);
-stack.pop();
-console.log(stack);
+let queue = [];
+function queue_remove_number(array) {
+    if (array.length >= 1) {
+        array.shift();
+    }
+    return array;
+}
+add_number(queue, 4);
+add_number(queue, 5);
+add_number(queue, 6);
+queue_remove_number(queue);
+console.log(queue);
--- Cham(self-stydy)/cham/lesson/src/task_20231025.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231025.ts
@@ -22,17 +22,43 @@
 // In the end, pop out the topmost element only once and display the stack.
 
 let stack: number[] = [];
-stack.push(3);
-console.log(stack); // [3]
 
-stack.push(6);
-console.log(stack); // [3,6]
+function add_number(array: number[], number: number): number[] {
+  if (array.length != 3 && typeof number != "string") {
+    array.push(number);
+  }
+  return array;
+}
 
-stack.push(8);
-console.log(stack); // [3,6,8]
+function stack_remove_number(array: number[]): number[] {
+  if (array.length >= 1) {
+    array.pop();
+  }
+  return array;
+}
 
-stack.pop();
-console.log(stack); //[3,6]
+add_number(stack, 1);
+add_number(stack, 2);
+add_number(stack, 3);
+add_number(stack, 3);
+stack_remove_number(stack);
 
-stack.pop();
-console.log(stack); //[3]
+console.log(stack);
+
+// 追加課題 queue
+// 最初に入る最初出る
+
+let queue: number[] = [];
+
+function queue_remove_number(array: number[]): number[] {
+  if (array.length >= 1) {
+    array.shift();
+  }
+  return array;
+}
+
+add_number(queue, 4);
+add_number(queue, 5);
+add_number(queue, 6);
+queue_remove_number(queue);
+console.log(queue);
