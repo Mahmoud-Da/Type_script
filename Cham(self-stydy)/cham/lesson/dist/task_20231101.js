73d47d920ea3dd38798cf320061e27fad4cb68c9
cham1235 <hana171819@gmail.com>
2023-11-02 22:06:23 +0900

    修正

--- Cham(self-stydy)/cham/lesson/dist/task_20231101.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231101.js
@@ -1,19 +1,19 @@
 "use strict";
-function sumNumber(number1, number2, number3) {
-    let sum = number1 + number2 + number3;
-    if (typeof number1 !== "number" &&
-        typeof number2 !== "number" &&
-        typeof number3 !== "number") {
+function sumNumber(array) {
+    if (typeof array !== "number" && array.length === 0) {
         return "数字を記入してください。";
     }
-    else if (sum > 100) {
-        let result = (number1 + number2 + number3) / 3;
-        let exlain = "その数は100を超えています。3つの数値の平均は";
-        return exlain + result;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        result = sum += array[i];
     }
-    return sum;
+    if (result > 100) {
+        let exlain = "その数は100を超えています。3つの数値の平均は";
+        return exlain + result / array.length;
+    }
+    return result;
 }
-sumNumber(1, 3, 0);
-sumNumber(0, 0, 0);
-sumNumber(18, 45, 40);
-sumNumber(8, 45, 40);
+sumNumber([1, 2, 5]);
+sumNumber([]);
+sumNumber([18, 45, 40]);
+sumNumber([8, 45, 40]);
--- Cham(self-stydy)/cham/lesson/src/task_20231101.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231101.ts
@@ -4,28 +4,22 @@
 
 // Create an own exception such that if variable sum is greater than 100 it should raise an exception.
 // Note: the output should print "The number is greater than 100.".
-function sumNumber(
-  number1: number,
-  number2: number,
-  number3: number
-): number | string {
-  let sum: number = number1 + number2 + number3;
-  if (
-    typeof number1 !== "number" &&
-    typeof number2 !== "number" &&
-    typeof number3 !== "number"
-  ) {
+function sumNumber(array: number[]): number | string {
+  if (typeof array !== "number" && array.length === 0) {
     return "数字を記入してください。";
-  } else if (sum > 100) {
-    let result: number = (number1 + number2 + number3) / 3;
-    let exlain: string = "その数は100を超えています。3つの数値の平均は";
-    return exlain + result;
   }
-  return sum;
+  let sum: number = 0;
+  for (let i = 0; i < array.length; i++) {
+    result = sum += array[i];
+  }
+  if (result > 100) {
+    let exlain: string = "その数は100を超えています。3つの数値の平均は";
+    return exlain + result / array.length;
+  }
+  return result;
 }
-
 // test
-sumNumber(1, 3, 0);
-sumNumber(0, 0, 0);
-sumNumber(18, 45, 40);
-sumNumber(8, 45, 40);
+sumNumber([1, 2, 5]);
+sumNumber([]);
+sumNumber([18, 45, 40]);
+sumNumber([8, 45, 40]);
