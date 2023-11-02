e2776285bcd61de3689840ff839537838005a207
cham1235 <hana171819@gmail.com>
2023-11-02 22:46:29 +0900

    修正

--- Cham(self-stydy)/cham/lesson/dist/task_20231101.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231101.js
@@ -1,19 +1,19 @@
 "use strict";
-function sumNumber(array) {
-    if (typeof array !== "number" && array.length === 0) {
+function sumNumber(...args) {
+    if (typeof args !== "number" && args.length === 0) {
         return "数字を記入してください。";
     }
     let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        result = sum += array[i];
+    for (let i = 0; i < args.length; i++) {
+        result = sum += args[i];
     }
     if (result > 100) {
         let exlain = "その数は100を超えています。3つの数値の平均は";
-        return exlain + result / array.length;
+        return exlain + result / args.length;
     }
     return result;
 }
-sumNumber([1, 2, 5]);
-sumNumber([]);
-sumNumber([18, 45, 40]);
-sumNumber([8, 45, 40]);
+sumNumber(1, 2, 5);
+sumNumber();
+sumNumber(18, 45, 40);
+sumNumber(8, 45, 40);
--- Cham(self-stydy)/cham/lesson/src/task_20231101.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231101.ts
@@ -4,22 +4,22 @@
 
 // Create an own exception such that if variable sum is greater than 100 it should raise an exception.
 // Note: the output should print "The number is greater than 100.".
-function sumNumber(array: number[]): number | string {
-  if (typeof array !== "number" && array.length === 0) {
+function sumNumber(...args: number[]): number | string {
+  if (typeof args !== "number" && args.length === 0) {
     return "数字を記入してください。";
   }
   let sum: number = 0;
-  for (let i = 0; i < array.length; i++) {
-    result = sum += array[i];
+  for (let i = 0; i < args.length; i++) {
+    result = sum += args[i];
   }
   if (result > 100) {
     let exlain: string = "その数は100を超えています。3つの数値の平均は";
-    return exlain + result / array.length;
+    return exlain + result / args.length;
   }
   return result;
 }
 // test
-sumNumber([1, 2, 5]);
-sumNumber([]);
-sumNumber([18, 45, 40]);
-sumNumber([8, 45, 40]);
+sumNumber(1, 2, 5);
+sumNumber();
+sumNumber(18, 45, 40);
+sumNumber(8, 45, 40);
