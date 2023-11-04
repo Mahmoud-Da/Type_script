d5fda3bf8c1761f8301d27e079dff2d6d9ee4aa2
cham1235 <hana171819@gmail.com>
2023-11-04 11:29:44 +0900

    完成

---
+++ Cham(self-stydy)/cham/lesson/dist/task_20231104.js
@@ -0,0 +1,22 @@
+"use strict";
+function removeCharacterString(string, character) {
+    if (typeof string != "string" ||
+        typeof string != "string" ||
+        string.length === 0 ||
+        character.length === 0) {
+        return "Enter string here, please!";
+    }
+    let array = string.split("");
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== character) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray.join("");
+}
+removeCharacterString("", "");
+removeCharacterString("HelloWorld", "");
+removeCharacterString("", "l");
+removeCharacterString("HelloWorld", "l");
+removeCharacterString("McDonalds", "s");
---
+++ Cham(self-stydy)/cham/lesson/src/task_20231104.ts
@@ -0,0 +1,36 @@
+// 課題26
+// 文字列と文字を入力として受け取り、文字列からその文字を削除するプログラムを書いてください。
+
+// 例-1：
+// もし与えられた文字列が「HelloWorld」で、削除する文字が 'l' である場合、出力は次のようになります：
+// HeoWord
+// 例-2：
+// もし与えられた文字列が「McDonalds」で、削除する文字が 's' である場合、出力は次のようになります：
+// McDonald」
+
+// Write a program which takes a string and a character as inputs and remove the character from the string.
+function removeCharacterString(string: string, character: string): string {
+  if (
+    typeof string != "string" ||
+    typeof string != "string" ||
+    string.length === 0 ||
+    character.length === 0
+  ) {
+    return "Enter string here, please!";
+  }
+  let array: string[] = string.split("");
+  let newArray: string[] = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== character) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray.join("");
+}
+
+// text
+removeCharacterString("", "");
+removeCharacterString("HelloWorld", "");
+removeCharacterString("", "l");
+removeCharacterString("HelloWorld", "l");
+removeCharacterString("McDonalds", "s");
