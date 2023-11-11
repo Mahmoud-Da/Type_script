57be15c2a144700b535cfe32c9590571982d7311
cham1235 <hana171819@gmail.com>
2023-11-11 13:30:10 +0900

    完成

--- Cham(self-stydy)/cham/lesson/dist/task_20231106.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231106.js
@@ -1,22 +1,58 @@
 "use strict";
-function timeForMilkAndCookies(years, month, date) {
-  if (!number) {
-    return "Enter number here, please!";
-  }
-  if (years != 0) {
+function timeForMilkAndCookies(years, months, dates) {
     let isChristmas = false;
-    switch (month) {
-      case 11:
-        if (date === 24) {
-          isChristmas = true;
+    if (typeof years != "number" ||
+        typeof months != "number" ||
+        typeof dates != "number" ||
+        years === 0 ||
+        months === 0 ||
+        dates === 0) {
+        return "Enter number here, please!";
+    }
+    else if (years != 0 && months != 0 && dates != 0) {
+        if (months === 11 && dates === 24) {
+            return (isChristmas = true);
         }
-        break;
-      default:
-        isChristmas = false;
-        break;
     }
     return isChristmas;
-  }
 }
+class date {
+    constructor(year, month, date) {
+        this.year = year;
+        this.month = month;
+        this.date = date;
+        this.year = year;
+        this.month = month;
+        this.date = date;
+    }
+    is_christmas() {
+        if (typeof this.year != "number" ||
+            typeof this.month != "number" ||
+            typeof this.date != "number" ||
+            this.year === 0 ||
+            this.month === 0 ||
+            this.date === 0) {
+            return "Enter number here, please!";
+        }
+        switch (this.month) {
+            case 11:
+                if (this.date === 24) {
+                    return true;
+                }
+                break;
+            default:
+                return false;
+        }
+    }
+}
+let Date1 = new date(2013, 11, 24);
+console.log(Date1.is_christmas());
+let Date2 = new date(3000, 12, 24);
+console.log(Date2.is_christmas());
 timeForMilkAndCookies(2013, 11, 13);
 timeForMilkAndCookies(2013, 11, 24);
+timeForMilkAndCookies(0, 11, 24);
+timeForMilkAndCookies(2013, 0, 24);
+timeForMilkAndCookies(2013, 11, 0);
+timeForMilkAndCookies(1889, 11, 24);
+timeForMilkAndCookies(1889, 1, 24);
--- Cham(self-stydy)/cham/lesson/src/task_20231106.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231106.ts
@@ -16,26 +16,68 @@
 
 function timeForMilkAndCookies(
   years: number,
-  month: number,
-  date: number
+  months: number,
+  dates: number
 ): boolean | string {
-  if (!number) {
-    return "Enter number here, please!";
-  }
   let isChristmas: boolean = false;
-  switch (month) {
-    case 11:
-      if (date === 24) {
-        isChristmas = true;
-      }
-      break;
-    default:
-      isChristmas = false;
-      break;
+  if (
+    typeof years != "number" ||
+    typeof months != "number" ||
+    typeof dates != "number" ||
+    years === 0 ||
+    months === 0 ||
+    dates === 0
+  ) {
+    return "Enter number here, please!";
+  } else if (years != 0 && months != 0 && dates != 0) {
+    if (months === 11 && dates === 24) {
+      return (isChristmas = true);
+    }
   }
   return isChristmas;
 }
 
+class date {
+  constructor(public year: number, public month: number, public date: number) {
+    this.year = year;
+    this.month = month;
+    this.date = date;
+  }
+  is_christmas() {
+    if (
+      typeof this.year != "number" ||
+      typeof this.month != "number" ||
+      typeof this.date != "number" ||
+      this.year === 0 ||
+      this.month === 0 ||
+      this.date === 0
+    ) {
+      return "Enter number here, please!";
+    }
+    switch (this.month) {
+      case 11:
+        if (this.date === 24) {
+          return true;
+        }
+        break;
+      default:
+        return false;
+    }
+  }
+}
+
+//test
+let Date1 = new date(2013, 11, 24);
+console.log(Date1.is_christmas());
+
+let Date2 = new date(3000, 12, 24);
+console.log(Date2.is_christmas());
+
 //test
 timeForMilkAndCookies(2013, 11, 13);
 timeForMilkAndCookies(2013, 11, 24);
+timeForMilkAndCookies(0, 11, 24);
+timeForMilkAndCookies(2013, 0, 24);
+timeForMilkAndCookies(2013, 11, 0);
+timeForMilkAndCookies(1889, 11, 24);
+timeForMilkAndCookies(1889, 1, 24);
