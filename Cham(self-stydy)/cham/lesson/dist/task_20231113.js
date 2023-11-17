e619b9fd76cb0f5809c3fb679c3d18a16187fc4c
cham1235 <hana171819@gmail.com>
2023-11-17 19:35:14 +0900

    修正

--- Cham(self-stydy)/cham/lesson/dist/task_20231113.js
+++ Cham(self-stydy)/cham/lesson/dist/task_20231113.js
@@ -3,9 +3,11 @@ function strings(...args) {
     if (typeof args !== "string" && args.length === 0) {
         return "文字を記入してください。";
     }
+    console.log("****************");
     for (let i = 0; i < args.length; i++) {
         console.log("・" + args[i] + " *");
     }
+    console.log("****************");
     return args;
 }
 strings("anh", "ban", "toi");
--- Cham(self-stydy)/cham/lesson/src/task_20231113.ts
+++ Cham(self-stydy)/cham/lesson/src/task_20231113.ts
@@ -15,9 +15,11 @@ function strings(...args: string[]): string | string[] {
   if (typeof args !== "string" && args.length === 0) {
     return "文字を記入してください。";
   }
+  console.log("****************");
   for (let i = 0; i < args.length; i++) {
     console.log("・" + args[i] + " *");
   }
+  console.log("****************");
   return args;
 }
 
